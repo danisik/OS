0af43b72f9450b68bc4fd0f6ee4941e7563f4de4
Vojtěch Danišík <32537562+danisik@users.noreply.github.com>
2019-11-12 23:42:18 +0100

    Thread handlers are now stored in map.

--- src/kernel/thread.h
+++ src/kernel/thread.h
@@ -4,6 +4,7 @@
 #include "state.h"
 
 #include <thread>
+#include <map>
 #include <condition_variable>
 
 class Thread {
@@ -14,8 +15,7 @@ public:
 	kiv_hal::TRegisters registers;
 	uint32_t exit_code;
 	kiv_os::TThread_Proc entry_point;
-	// TODO Thread: maybe create as map.
-	std::vector<kiv_os::TThread_Proc> handlers;
+	std::map<kiv_os::NSignal_Id, kiv_os::TThread_Proc> handlers;
 
 	Thread(kiv_os::TThread_Proc t_entry_point, kiv_hal::TRegisters t_registers);
 	~Thread();
