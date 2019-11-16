f1674002ea2b16c22514041c6eb2971894a8c538
Vojtěch Danišík <32537562+danisik@users.noreply.github.com>
2019-11-16 23:31:58 +0100

    echo updated.

--- src/user/echo.cpp
+++ src/user/echo.cpp
@@ -13,20 +13,12 @@ size_t __stdcall echo(const kiv_hal::TRegisters &regs) {
 
 			size_t written;
 			kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
-
-			uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
-			kiv_os_rtl::Exit(exit_code);
-			return 0;
 		}
 		else {
 			output.append("Echo is off.\n");
 
 			size_t written;
 			kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
-
-			uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
-			kiv_os_rtl::Exit(exit_code);
-			return 0;
 		}
 	}
 	else if (strcmp(arguments, ECHO_HELP) == 0) {
@@ -37,33 +29,20 @@ size_t __stdcall echo(const kiv_hal::TRegisters &regs) {
 
 		size_t written;
 		kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
-
-		uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
-		kiv_os_rtl::Exit(exit_code);
-		return 0;
 	}
 	else if (strcmp(arguments, ECHO_ON) == 0) {
 		echo_on = true;
-
-		uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
-		kiv_os_rtl::Exit(exit_code);
-		return 0;
 	}
 	else if (strcmp(arguments, ECHO_OFF) == 0) {
 		echo_on = false;
-
-		uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
-		kiv_os_rtl::Exit(exit_code);
-		return 0;
 	}
 	else {
 		size_t written;
 		output.append(arguments);
 		output.append("\n");
-		kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);
-
-		uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
-		kiv_os_rtl::Exit(exit_code);
-		return 0;
+		kiv_os_rtl::Write_File(std_out, output.data(), output.size(), written);	
 	}
+	uint16_t exit_code = static_cast<uint16_t>(kiv_os::NOS_Error::Success);
+	kiv_os_rtl::Exit(exit_code);
+	return 0;
 }
