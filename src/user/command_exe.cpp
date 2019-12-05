c6d78b7d5339d53c9160be63b9f0f7bfe9ae9875
Vojtěch Danišík <32537562+danisik@users.noreply.github.com>
2019-12-05 18:40:49 +0100

    Cleaning project solution.

--- src/user/command_exe.cpp
+++ src/user/command_exe.cpp
@@ -25,7 +25,7 @@ void command_exe::Execute_Commands(std::vector<command_parser::Command> commands
 		if (command.base != "cd" && command.base != "echo" && command.base != "tasklist" && command.base != "rd"
 			&& command.base != "md" && command.base != "type" && command.base != "find" && command.base != "sort"
 			&& command.base != "dir" && command.base != "rgen" && command.base != "freq" && command.base != "shell"
-			&& command.base != "shutdown" && command.base != "exit" && command.base != "test") {
+			&& command.base != "shutdown" && command.base != "exit") {
 
 			std::string output = "Unknown command.\n";
 			size_t written;
